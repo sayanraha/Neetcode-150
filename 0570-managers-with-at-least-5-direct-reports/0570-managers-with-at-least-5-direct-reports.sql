32444bef3a87a8ed6840a932d314658d60cabaf3
Sayan Raha <109590974+sayanraha@users.noreply.github.com>
2026-01-12 19:21:16 +0530

    Time: 332 ms (54.09%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+
+-- SELECT e2.name
+-- FROM Employee e1 
+-- JOIN Employee e2
+-- ON e1.managerId = e2.id
+-- GROUP BY e2.id, e2.name
+-- HAVING count(e1.id >=5);
+
+
+SELECT e2.name
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
+GROUP BY e2.id, e2.name
+HAVING COUNT(e1.id) >= 5;
